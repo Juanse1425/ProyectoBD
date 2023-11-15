cb45495bab6ed92a4d4e9656616ede59a3af04fa
Juan Sebastian Ortiz Ramirez <94332942+Juanse1425@users.noreply.github.com>
2023-11-15 09:09:20 -0500

    Mejoras en la clase Conexion

--- src/Conexion.java
+++ src/Conexion.java
@@ -2,69 +2,36 @@ import java.sql.*;
 
 public class Conexion {
 
-    String url = "jdbc:mysql://localhost:3306/proyectobases";
-    String username = "root";
-    String password = "root";
-    Connection connection;
+    public Conexion() {}
 
-    public Conexion() {
-
-    }
-
-    public Connection conectar() {
-        try {
-            connection = DriverManager.getConnection(url, username, password);
-            if (connection != null) {
-                System.out.println("Conexión exitosa a la base de datos.");
-            }
-        } catch (SQLException e) {
-            System.out.println("Error al conectar a la base de datos: " + e.getMessage());
-        }
-        return connection;
-    }
-
-    public void desconectar() {
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            System.out.println("Error al desconectar de la base de datos: " + e.getMessage());
-        }
+    public Connection conectar() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/proyectobases";
+        String username = "root";
+        String password = "root";
+        return DriverManager.getConnection(url, username, password);
     }
 
     public boolean verificarID(String consulta) {
-        boolean idExistente = false;
+        try (Connection conexion = conectar();
+             PreparedStatement statement = conexion.prepareStatement(consulta);
+             ResultSet resultSet = statement.executeQuery()) {
 
-        try {
-            Connection conexion = conectar();
-            PreparedStatement statement = conexion.prepareStatement(consulta);
-            ResultSet resultSet = statement.executeQuery();
-
-            // Verificar si hay resultados y si el total es mayor que cero
             if (resultSet.next()) {
                 int total = resultSet.getInt("total");
-                if (total > 0) {
-                    idExistente = true; // El ID ya existe en la tabla
-                }
+                return total > 0;
             }
-            // Cerrar recursos
-            resultSet.close();
-            statement.close();
         } catch (SQLException e) {
+            // Manejar la excepción adecuadamente según el contexto
             e.printStackTrace();
         }
-        return idExistente;
+        return false;
     }
 
     public void ejecutarSentenciaSQL(String sentenciaSQL) {
-        Connection conexion = null;
-        PreparedStatement statement = null;
+        try (Connection conexion = conectar();
+             PreparedStatement statement = conexion.prepareStatement(sentenciaSQL)) {
 
-        try {
-            conexion = conectar();
-            statement = conexion.prepareStatement(sentenciaSQL);
-
-            // Ejecutar la sentencia SQL para una inserción
-            int filasAfectadas = statement.executeUpdate(); // Ejecutar un INSERT, UPDATE o DELETE
+            int filasAfectadas = statement.executeUpdate();
 
             if (filasAfectadas > 0) {
                 System.out.println("Inserción exitosa. Se han insertado " + filasAfectadas + " filas.");
@@ -72,38 +39,16 @@ public class Conexion {
                 System.out.println("No se ha realizado ninguna inserción.");
             }
         } catch (SQLException e) {
+            // Manejar la excepción adecuadamente según el contexto
             e.printStackTrace();
-        } finally {
-            // Cerrar recursos en el bloque finally
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-                if (conexion != null) {
-                    conexion.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
 
     public void mostrarResultadosEnConsola(String consulta) {
-        Connection conexion = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
+        try (Connection conexion = conectar();
+             Statement statement = conexion.createStatement();
+             ResultSet resultSet = statement.executeQuery(consulta)) {
 
-        try {
-            // Establecer conexión con la base de datos
-            conexion = conectar();
-
-            // Crear una declaración (statement)
-            statement = conexion.createStatement();
-
-            // Ejecutar la consulta SQL
-            resultSet = statement.executeQuery(consulta);
-
-            // Iterar a través de los resultados y mostrar en la consola
             ResultSetMetaData metaData = resultSet.getMetaData();
             int columnCount = metaData.getColumnCount();
 
@@ -114,23 +59,8 @@ public class Conexion {
                 System.out.println();
             }
         } catch (SQLException e) {
+            // Manejar la excepción adecuadamente según el contexto
             e.printStackTrace();
-        } finally {
-            // Cerrar ResultSet, Statement y Connection en el bloque finally
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-                if (statement != null) {
-                    statement.close();
-                }
-                if (conexion != null) {
-                    conexion.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
     }
-
 }
