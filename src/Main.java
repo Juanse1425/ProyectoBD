2dc0cedd756770c7c5cba48e60e2b847bb61b7d4
Juan Sebastian Ortiz Ramirez <94332942+Juanse1425@users.noreply.github.com>
2023-11-15 09:19:28 -0500

    Mejoras en la clases Main y Reporte

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,49 @@
-import javax.swing.*;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
+            int opcion;
+            do {
+                opcion = mostrarMenu();
+                ejecutarAccion(opcion);
+            } while (opcion != 5);
+    }
+
+    private static int mostrarMenu() {
         int opcion = 0;
-        do {
-            try {
-                opcion = Integer.parseInt(JOptionPane.showInputDialog("¿Que desea hacer?\n\n" +
-                        "1. Crear Examen\n" +
-                        "2. Agregar Preguntas\n" +
-                        "3. Presentar Examen\n" +
-                        "4. Reportes\n" +
-                        "5. Salir"));
-                if(opcion >= 1 && opcion <= 5){
-                    switch (opcion){
-                        case 1:
-                            new Examen();
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            new Reporte();
-                            break;
-                    }
-                }else{
-                    JOptionPane.showMessageDialog(null, "Ingrese una opcion valida");
-                }
-            } catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(null, "Ingrese una opcion valida");
+        try {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("""
+                    ¿Qué desea hacer?
+
+                    1. Crear Examen
+                    2. Agregar Preguntas
+                    3. Presentar Examen
+                    4. Reportes
+                    5. Salir"""));
+
+            if (opcion < 1 || opcion > 5) {
+                JOptionPane.showMessageDialog(null, "Ingrese una opción válida");
             }
-        } while (opcion != 5);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Ingrese una opción válida");
+        }
+        return opcion;
+    }
+
+    private static void ejecutarAccion(int opcion) {
+        switch (opcion) {
+            case 1:
+                new Examen();
+                break;
+            case 2:
+                // Lógica para agregar preguntas
+                break;
+            case 3:
+                // Lógica para presentar examen
+                break;
+            case 4:
+                new Reporte();
+                break;
+        }
     }
 }
--- src/Reporte.java
+++ src/Reporte.java
@@ -1,104 +1,117 @@
 import javax.swing.*;
 
 public class Reporte {
-    private Conexion conexion = new Conexion();
+    private final Conexion conexion = new Conexion();
+
     public Reporte() {
-        String consulta;
-        int opcion = 0;
+        int opcion;
         do {
-            try {
-                opcion = Integer.parseInt(JOptionPane.showInputDialog("¿Que desea hacer?\n\n" +
-                        "1. Reporte 1\n" +
-                        "2. Reporte 2\n" +
-                        "3. Reporte 3\n" +
-                        "4. Reporte 4\n" +
-                        "5. Reporte 5\n" +
-                        "6. Reporte 6\n" +
-                        "7. Reporte 7\n" +
-                        "8. Volver"));
-                if(opcion >= 1 && opcion <= 8){
-                    switch (opcion){
-                        case 1:
-                            consulta = """
-                                    SELECT resp.*, est.*
-                                    FROM examen exam
-                                    JOIN respuesta resp ON exam.idExamen = resp.idExamen
-                                    JOIN estudiante est ON est.dni_Estudiante = resp.dni_Estudiante;""";
-                            conexion.mostrarResultadosEnConsola(consulta);
-                            break;
-                        case 2:
-                            consulta = """
-                                    SELECT AVG(resp.calificacion) AS promedioCalificacion
-                                    FROM examen exam
-                                    JOIN respuesta resp ON exam.idExamen = resp.idExamen
-                                    JOIN estudiante est ON est.dni_Estudiante = resp.dni_Estudiante;""";
-                            conexion.mostrarResultadosEnConsola(consulta);
-                            break;
-                        case 3:
-                            consulta = """
-                                    SELECT exam.*
-                                    FROM examen exam
-                                    JOIN horario hor ON hor.idHorario = exam.idHorario
-                                    WHERE MONTH(hor.fechaExamen) > 6;""";
-                            conexion.mostrarResultadosEnConsola(consulta);
-                            break;
-                        case 4:
-                            consulta = """
-                                    SELECT exam.idExamen , AVG(resp.calificacion) AS promedioCalificacionPregunta
-                                    FROM examen exam
-                                    JOIN respuesta resp ON exam.idExamen = resp.idExamen
-                                    JOIN estudiante est ON est.dni_Estudiante = resp.dni_Estudiante
-                                    GROUP BY exam.idExamen;""";
-                            conexion.mostrarResultadosEnConsola(consulta);
-                            break;
-                        case 5:
-                            consulta = """
-                                    SELECT cur.*, und.idPlanEstudio, tem.codigoUnidad, und.nombre, und.descripcion, tem.idTema, tem.nombre
-                                    FROM tema tem
-                                    JOIN unidades und ON tem.codigoUnidad = und.codigo
-                                    JOIN planestudio plan ON und.idPlanEstudio = plan.id
-                                    JOIN curso cur ON plan.codigoCurso = cur.codigo;""";
-                            conexion.mostrarResultadosEnConsola(consulta);
-                            break;
-                        case 6:
-                            consulta = """
-                                    SELECT est.dni_Estudiante, (SUM(examEst.calificacion)/COUNT(*)) AS CalificacionFinal,
-                                    CASE
-                                    \tWHEN (SUM(examEst.calificacion) / COUNT(*)) < 60 THEN 'Perdio'
-                                    \tELSE 'Gano'
-                                    END AS resultado
-                                    FROM examenestudiante examEst
-                                    JOIN estudiante est ON examEst.dni_Estudiante = est.dni_Estudiante
-                                    GROUP BY examEst.dni_Estudiante;""";
-                            conexion.mostrarResultadosEnConsola(consulta);
-                            break;
-                        case 7:
-                            consulta = """
-                                    SELECT examEst.idExamen, MIN(examEst.calificacion) as calificacion_minima,
-                                        (SELECT est_min.nombre
-                                         FROM examenestudiante examEst_min
-                                         JOIN estudiante est_min ON examEst_min.dni_Estudiante = est_min.dni_Estudiante
-                                         WHERE examEst_min.idExamen = examEst.idExamen
-                                         AND examEst_min.calificacion = MIN(examEst.calificacion)
-                                         LIMIT 1) as estudiante_minimo, MAX(examEst.calificacion) as calificacion_maxima,
-                                        (SELECT est_max.nombre
-                                         FROM examenestudiante examEst_max
-                                         JOIN estudiante est_max ON examEst_max.dni_Estudiante = est_max.dni_Estudiante
-                                         WHERE examEst_max.idExamen = examEst.idExamen
-                                         AND examEst_max.calificacion = MAX(examEst.calificacion)
-                                         LIMIT 1) as estudiante_maximo
-                                    FROM examenestudiante examEst
-                                    JOIN estudiante est ON examEst.dni_Estudiante = est.dni_Estudiante
-                                    GROUP BY examEst.idExamen;""";
-                            conexion.mostrarResultadosEnConsola(consulta);
-                            break;
-                    }
-                }else{
-                    JOptionPane.showMessageDialog(null, "Ingrese una opcion valida");
-                }
-            } catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(null, "Ingrese una opcion valida");
-            }
+            opcion = mostrarMenu();
+            ejecutarReporte(opcion);
         } while (opcion != 8);
+    }
+
+    private static int mostrarMenu() {
+        int opcion = 0;
+        try {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("""
+                    ¿Que desea hacer?
+
+                    1. Reporte 1
+                    2. Reporte 2
+                    3. Reporte 3
+                    4. Reporte 4
+                    5. Reporte 5
+                    6. Reporte 6
+                    7. Reporte 7
+                    8. Volver"""));
+
+            if (opcion < 1 || opcion > 8) {
+                JOptionPane.showMessageDialog(null, "Ingrese una opción válida");
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Ingrese una opción válida");
+        }
+        return opcion;
+    }
+
+    private void ejecutarReporte(int opcion) {
+        String consulta;
+        switch (opcion) {
+            case 1:
+                consulta = """
+                        SELECT resp.*, est.*
+                        FROM examen exam
+                        JOIN respuesta resp ON exam.idExamen = resp.idExamen
+                        JOIN estudiante est ON est.dni_Estudiante = resp.dni_Estudiante;""";
+                conexion.mostrarResultadosEnConsola(consulta);
+                break;
+            case 2:
+                consulta = """
+                        SELECT AVG(resp.calificacion) AS promedioCalificacion
+                        FROM examen exam
+                        JOIN respuesta resp ON exam.idExamen = resp.idExamen
+                        JOIN estudiante est ON est.dni_Estudiante = resp.dni_Estudiante;""";
+                conexion.mostrarResultadosEnConsola(consulta);
+                break;
+            case 3:
+                consulta = """
+                        SELECT exam.*
+                        FROM examen exam
+                        JOIN horario hor ON hor.idHorario = exam.idHorario
+                        WHERE MONTH(hor.fechaExamen) > 6;""";
+                conexion.mostrarResultadosEnConsola(consulta);
+                break;
+            case 4:
+                consulta = """
+                        SELECT exam.idExamen , AVG(resp.calificacion) AS promedioCalificacionPregunta
+                        FROM examen exam
+                        JOIN respuesta resp ON exam.idExamen = resp.idExamen
+                        JOIN estudiante est ON est.dni_Estudiante = resp.dni_Estudiante
+                        GROUP BY exam.idExamen;""";
+                conexion.mostrarResultadosEnConsola(consulta);
+                break;
+            case 5:
+                consulta = """
+                        SELECT cur.*, und.idPlanEstudio, tem.codigoUnidad, und.nombre, und.descripcion, tem.idTema, tem.nombre
+                        FROM tema tem
+                        JOIN unidades und ON tem.codigoUnidad = und.codigo
+                        JOIN planestudio plan ON und.idPlanEstudio = plan.id
+                        JOIN curso cur ON plan.codigoCurso = cur.codigo;""";
+                conexion.mostrarResultadosEnConsola(consulta);
+                break;
+            case 6:
+                consulta = """
+                        SELECT est.dni_Estudiante, (SUM(examEst.calificacion)/COUNT(*)) AS CalificacionFinal,
+                        CASE
+                        \tWHEN (SUM(examEst.calificacion) / COUNT(*)) < 60 THEN 'Perdio'
+                        \tELSE 'Gano'
+                        END AS resultado
+                        FROM examenestudiante examEst
+                        JOIN estudiante est ON examEst.dni_Estudiante = est.dni_Estudiante
+                        GROUP BY examEst.dni_Estudiante;""";
+                conexion.mostrarResultadosEnConsola(consulta);
+                break;
+            case 7:
+                consulta = """
+                        SELECT examEst.idExamen, MIN(examEst.calificacion) as calificacion_minima,
+                            (SELECT est_min.nombre
+                             FROM examenestudiante examEst_min
+                             JOIN estudiante est_min ON examEst_min.dni_Estudiante = est_min.dni_Estudiante
+                             WHERE examEst_min.idExamen = examEst.idExamen
+                             AND examEst_min.calificacion = MIN(examEst.calificacion)
+                             LIMIT 1) as estudiante_minimo, MAX(examEst.calificacion) as calificacion_maxima,
+                            (SELECT est_max.nombre
+                             FROM examenestudiante examEst_max
+                             JOIN estudiante est_max ON examEst_max.dni_Estudiante = est_max.dni_Estudiante
+                             WHERE examEst_max.idExamen = examEst.idExamen
+                             AND examEst_max.calificacion = MAX(examEst.calificacion)
+                             LIMIT 1) as estudiante_maximo
+                        FROM examenestudiante examEst
+                        JOIN estudiante est ON examEst.dni_Estudiante = est.dni_Estudiante
+                        GROUP BY examEst.idExamen;""";
+                conexion.mostrarResultadosEnConsola(consulta);
+                break;
+        }
     }
 }
